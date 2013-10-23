85fd875ae2c6ef7420bd8f185e004491756051bc
Alex Popelard <apopelard2014@kellogg.northwestern.edu>
2013-10-23 00:02:11 -0500

    Created FortunesController
    - add fortune_controller.rb
    - create FortuneController class
    - add randon number Ruby code

---
+++ app/controllers/fortunes_controller.rb
@@ -0,0 +1,6 @@
+class FortunesController < ApplicationController
+  def numbers
+    @lucky_numbers = [rand(1..100), rand(1..100), rand(1..100), rand(1..100), rand(1..100)]
+    render("numbers")
+  end
+end
